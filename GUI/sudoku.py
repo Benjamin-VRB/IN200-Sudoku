39235d2a03bb46aa5a0cd266f8a4cce7c68d0470
Benjamin VRBANAC <180460887+Benjamin-VRB@users.noreply.github.com>
2026-03-09 00:26:15 +0100

    Fichier .py pour tester la grille de Sudoku

---
+++ GUI/sudoku.py
@@ -0,0 +1,21 @@
+"""
+import tkinter as tk
+
+from GUI.fenetre import LARGEUR_PIXEL_FENETRE, HAUTEUR_PIXEL_FENETRE
+from GUI.animations import mouvement_exterieur_fond_menu
+from GUI.widgets import creer_grille_sudoku
+
+
+def aller_sudoku(canvas: tk.Canvas) -> None:
+    
+    mouvement_exterieur_fond_menu(canvas)
+
+    LONGUEUR_COTE_CASE: int = 60
+    NB_CASE_COTE: int = 9
+    NB_CARRE_COTE: int = 3
+    LONGUEUR_COTE_GRILLE: int = LONGUEUR_COTE_CASE * NB_CASE_COTE
+    X_GRILLE: int = (LARGEUR_PIXEL_FENETRE - LONGUEUR_COTE_GRILLE) // 2
+    Y_GRILLE: int = (HAUTEUR_PIXEL_FENETRE - LONGUEUR_COTE_GRILLE) // 2
+    creer_grille_sudoku(canvas, tag="grille_sudoku", coord=(X_GRILLE, Y_GRILLE), nb_case_cote=NB_CASE_COTE, 
+                        longueur_cote_case=LONGUEUR_COTE_CASE, nb_carre_cote=NB_CARRE_COTE)
+"""
