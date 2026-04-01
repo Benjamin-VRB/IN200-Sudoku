3b33012e87060e11e2f0e429f3e8f284fc9ed571
Joseph Houel <joseph.houel00@gmail.com>
2026-04-01 08:51:41 +0200

    Ajout de l'ancienne fonction de vérification

--- Grille/Archive.py
+++ Grille/Archive.py
@@ -855,3 +855,49 @@ def suppression_valeur(grille_a_vider : list[list:int], nombre_valeurs_a_supprim
             else:
                 break # sinon il s'arrete la et renvoie la grille comme elle est
     return grille_vidée
+
+def verification_condition_sudoku(grille: list[list[int]], coord: tuple[int, int]):
+    """Vérifie que la valeur de la case venant 'être saisie vérifie les règles du sudoku.
+    
+    Entrée:
+        La grille de sudoku ainsi que sa dimension et les coordonnées de la case venant d'ête remplie 
+        par l'utilisateur.
+    
+    Sortie:
+        Liste des coordonnées des cases pour lesquelles cela les règles du sudoku ne sont pas respectées."""
+   
+    dimension = len(grille)
+    liste_cases_invalides = []     
+
+    ligne = coord[0]
+    colonne = coord[1]
+    valeur = grille[ligne][colonne]
+
+    liste_indices_lignes = [i for i in range(dimension)]   # Liste des indices des lignes a tester.
+    liste_indices_lignes.remove(ligne)                     # a laquelle on enlève l'indice de la case venant d'être remplie.
+
+    for i in liste_indices_lignes:
+        if grille[i][colonne] == valeur:
+            liste_cases_invalides.append((i, colonne))     # On regarde dans toute la ligne.
+
+
+    liste_indices_colonnes = [i for i in range(dimension)]  # Liste des indices des colonnes a tester.
+    liste_indices_colonnes.remove(colonne)                  # a laquelle on enlève l'indice de la case venant d'être remplie.
+
+    for i in liste_indices_colonnes:
+        if grille[ligne][i] == valeur:
+            liste_cases_invalides.append((ligne, i))        # On regarde dans toute la colonne
+
+
+    taille_bloc = int(math.sqrt(dimension))                 # calcul de la taille d'un carré
+
+    debut_ligne = (ligne // taille_bloc) * taille_bloc          # On regarde dans quel bloc se trouve la case qu'on vient de remplir
+    debut_colonne = (colonne // taille_bloc) * taille_bloc
+
+    for i in range(debut_ligne, debut_ligne + taille_bloc):           # On teste toutes les cases de ce bloc.
+        for j in range(debut_colonne, debut_colonne + taille_bloc):
+            if (i, j) != coord and grille[i][j] == valeur:
+                liste_cases_invalides.append((i, j))
+
+    return liste_cases_invalides                            # On renvoie la liste des coordonnées des cases a faire voir au joueur.
+
