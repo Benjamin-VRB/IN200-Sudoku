22dc2c83d0f37edea72d738118892c042a79a3be
Joseph Houel <joseph.houel00@gmail.com>
2026-04-08 21:47:40 +0200

    Ajout de la fonction de vérification du Kenken.

--- Grille/verification.py
+++ Grille/verification.py
@@ -1,11 +1,16 @@
 import math
+from kenken import verifier_cage
+
+
+####       Fonctions "de base"       ####
+
 
 def verification_lignes_colonnes(grille: list[list[int]], coord: tuple[int, int], liste_cases_invalides):
     """"Vérifie que la valeur qui vient dêtre saisie vérifie les conditions lignes et colonnes du Sudoku.
     
     Entrée:
-        La grille de sudoku, les coordonnées de la case venant d'ête remplie, et une liste (qui sera une liste vide). 
-        par l'utilisateur."""
+        La grille de sudoku, les coordonnées de la case venant d'ête remplie, et une liste (qui sera 
+        une liste vide)."""
     
     dimension = len(grille)
     ligne = coord[0]
@@ -29,13 +34,12 @@ def verification_lignes_colonnes(grille: list[list[int]], coord: tuple[int, int]
 
 
 
-
 def verification_carre_Sudoku(grille: list[list[int]], coord: tuple[int, int], liste_cases_invalides):
     """Vérifie que la valeur qui vient dêtre saisie vérifie la condition carré du Sudoku.
     
     Entrée:
-        La grille de sudoku, les coordonnées de la case venant d'ête remplie, et une liste (qui sera une liste vide). 
-        par l'utilisateur."""
+        La grille de sudoku, les coordonnées de la case venant d'ête remplie, et une liste (qui sera 
+        une liste vide)."""
 
     dimension = len(grille)
     liste_cases_invalides = []     
@@ -57,18 +61,67 @@ def verification_carre_Sudoku(grille: list[list[int]], coord: tuple[int, int], l
 
 
 
+####       Sudoku classique       ####
 
 def verification_sudoku_classique_complet(grille: list[list[int]], coord: tuple[int, int]):
     """"Vérifie que la valeur qui vient dêtre saisie vérifie toutes les conditions du Sudoku.
     
     Entrée:
-        La grille de sudoku et  les coordonnées de la case venant d'ête remplie.
+        La grille de sudoku et les coordonnées de la case venant d'ête remplie.
         
     Sortie:
         Renvoie la liste de toutes les cases pour lesquelles cette valeur ne fonctionne pas."""
 
     liste_cases_invalides = []
-    verification_lignes_colonnes(grille, coord, liste_cases_invalides)
-    verification_carre_Sudoku(grille, coord, liste_cases_invalides)
+    verification_lignes_colonnes(grille, coord, liste_cases_invalides)  # Vérification des lignes et ds colonnes.
+    verification_carre_Sudoku(grille, coord, liste_cases_invalides)    # Vérification des carrés.
     return liste_cases_invalides
 
+
+
+
+
+
+
+
+
+####       Variante Kenken       ####
+
+
+def verification_cages_kenken(grille, dictionnaire_cages):
+    """Vérifie que les cages vérifient toujours les informations requises sur les opérations.
+    
+    Entrée:
+        La grille de sudoku ainsi que le dictionnaire contenant les informations sur les cages."""
+
+    for cage in dictionnaire_cages.values():
+        if not verifier_cage(grille, cage):
+            return cage
+    return True
+
+
+def verification_kenken(grille, coord, dictionnaire_cages):
+    """"Vérifie que la valeur qui vient dêtre saisie vérifie toutes les conditions du Kenken.
+    
+    Entrée:
+        La grille de sudoku, les coordonnées de la case venant d'ête remplie et un dictionnaire contenenant
+        les informations du kenken.
+        
+    Sortie:
+        Renvoie la liste de toutes les cases et cages pour lesquelles cette valeur ne fonctionne pas."""
+
+    liste_cases_invalides = []
+    verification_lignes_colonnes(grille, coord, liste_cases_invalides)      # vérification des lignes et des colonnes.
+    verif = verification_cages_kenken(grille, dictionnaire_cages)         # Vérifications des cages du kenken
+
+    if  liste_cases_invalides != [] and verif:         # traitement des différents cas.
+        return liste_cases_invalides
+    
+    elif liste_cases_invalides != []:
+        return liste_cases_invalides, verif
+    
+    elif liste_cases_invalides == [] and verif:
+        return
+    
+    elif liste_cases_invalides == []:
+        return verif
